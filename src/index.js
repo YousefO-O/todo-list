6ef30acf3e77244170a934c6e2d7a03712603d99
Yousef <ywalotaibi2010@gmail.com>
2026-05-21 15:58:49 +0300

    Add module to manipulate user data
    The closure will be handling the user data like adding a new project or
    removing a todo item. Stuff like that.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './styles.css';
+import { Project } from './project-class.js';
+import { TodoItem } from './todo-item-class.js';
+
+const dataController = (()=>{
+    const projects = [];
+    const createNewProject = (title) => {   
+        const newProject = new Project(title);
+        projects.push(newProject);
+    };
+    return {
+        createNewProject,
+    };
+})();
