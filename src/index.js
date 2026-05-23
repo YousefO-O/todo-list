067286a205c1f05b949f6ce194dcc74d94942c83
Yousef <ywalotaibi2010@gmail.com>
2026-05-23 17:22:15 +0300

    Revert "Merge branch 'main' of github.com:YousefO-O/todo-list"
    This reverts commit 458f4aacce96533c588216954e3a6aceddd9556b, reversing
    changes made to 010998d328b087297d1b546ffaf905bd93b3bc78.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import './styles.css';
 import { createNewProject } from './modules/dataController.js';
 import { createNewTodoItem } from './modules/dataController.js';
-import { deleteTodoItem } from './modules/dataController.js';
+
 // Default Project
 createNewProject('Todo List');
 createNewTodoItem('todo 1', 'todo 1 description', 'now', 'important');
-deleteTodoItem(/*id I need to enter*/);
--- src/modules/classes.js
+++ src/modules/classes.js
@@ -4,14 +4,6 @@ export class Project {
         this.todoItems = [];
         this.id = crypto.randomUUID();
     };
-    deleteTodoItem = (todoItem) => {
-        if(todoItem) {
-            const todoItemIndex = this.todoItems.indexOf(todoItem);
-            this.todoItems.splice(todoItemIndex, 1);
-            console.log(`Successfully deleted todo item: ${todoItem.title}` +
-                `from project: ${this.title}`);
-        };
-    };
 };
 
 export class TodoItem {
